5823e372414a22e92683c344bc9cb63dbf17571a
Tomasz Klimek <tomasz.klimek@yahoo.pl>
2020-03-27 13:09:38 +0100

    bar init

--- covid_19/apps/daily_dashboard.py
+++ covid_19/apps/daily_dashboard.py
@@ -30,6 +30,8 @@ STR_TO_DATE = {str_date: convert_date(str_date) for str_date in DAYS}
 # need to rename date columns to avoid constant conversions later
 [df.rename(columns=STR_TO_DATE, inplace=True) for df in [CONFIRMED, DEATHS, RECOVERED]]
 
+ALL_DFS = [CONFIRMED, DEATHS, RECOVERED]
+
 
 def draw_infection_map(df, day):
     df = df[['Province/State', 'Country/Region', 'Lat', 'Long', day]].copy()
@@ -43,6 +45,7 @@ def draw_infection_map(df, day):
             color='#D9534F'
         )
     )
+
 
     layout = go.Layout(
         margin=dict(l=0, r=0, t=0, b=0),
@@ -66,6 +69,36 @@ def draw_infection_map(df, day):
     return fig
 
 
+def draw_curve(df1, df2, day):
+    day_idx = list(df1.columns).index(day) + 1
+
+    df1_curve_trace = go.Bar(
+        x=DAYS,
+        y=list(np.sum(df1.iloc[:, 4:day_idx].copy(), axis=0)),
+        name='Confirmed',
+        marker=dict(
+            color='#d9534f')
+    )
+    df2_curve_trace = go.Bar(
+        x=DAYS,
+        y=list(np.sum(df2.iloc[:, 4:day_idx].copy(), axis=0)),
+        width=0.4,
+        name='Recovered',
+        marker=dict(
+            color='#5cb85c')
+    )
+    curve_layout = go.Layout(
+        margin=dict(l=0, r=0, t=0, b=0),
+        paper_bgcolor='#abb6c2',
+        plot_bgcolor='#abb6c2',
+        barmode='overlay',
+        xaxis=dict(
+            range=[DAYS[0], DAYS[-1]],
+            fixedrange=True))
+    fig = go.Figure([df1_curve_trace, df2_curve_trace], curve_layout)
+    return fig
+
+
 def render_kpi_card(covid_state, color):
     kpi_card = dbc.Col(dbc.Card(
         children=[
@@ -83,7 +116,7 @@ def render_kpi_card(covid_state, color):
                             dbc.Col(
                                 children=[
                                     html.Div('Daily Change', className='text-center'),
-                                    html.H1(id=f'{covid_state}-change', className='text-center')
+                                    html.Div(id=f'{covid_state}-change', className='display-4 text-center')
                                 ]
                             )
                         ]
@@ -104,6 +137,7 @@ kpi_row = dbc.Row(
         in zip(covid_states, ['danger', 'dark', 'success'])
         ]
 )
+
 
 charts_row = dbc.Row(
     children=[
@@ -142,7 +176,7 @@ content = dbc.Container(
         kpi_row,
         date_picker,
         charts_row,
-        run_animation_button
+        dbc.Col([dcc.Graph(figure=draw_curve(CONFIRMED, RECOVERED, '2020-03-26'))], width=6)
     ],
     fluid=True
 )
@@ -165,6 +199,19 @@ def update_map(day):
     [Output(f'{covid_state}-total', 'children') for covid_state in covid_states],
     [Input('date-picker-single', 'date')]
 )
-def update_confirmed_total(day):
+def update_total(day):
     date_only = day.split('T')[0]
-    return ['{:,.0f}'.format(np.sum(df[date_only])).replace(',', ' ') for df in [CONFIRMED, DEATHS, RECOVERED]]
+    return ['{:,.0f}'.format(np.sum(df[date_only])).replace(',', ' ')
+            for df in ALL_DFS]
+
+
+@app.callback(
+    [Output(f'{covid_state}-change', 'children') for covid_state in covid_states],
+    [Input('date-picker-single', 'date')]
+)
+def update_change(day):
+    date_only = day.split('T')[0]
+    selected_idx = [list(df.columns).index(date_only) for df in ALL_DFS]
+    results = [np.sum(df.iloc[:, idx]) - np.sum(df.iloc[:, idx-1])
+               for df, idx in zip(ALL_DFS, selected_idx)]
+    return ['{:+,.0f}'.format(result).replace(',', ' ') for result in results]
